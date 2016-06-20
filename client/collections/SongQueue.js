957a9d0d2c85e2d3a87448a307507ae50a76ff21
Justin <justinminh225@gmail.com>
2016-06-20 00:22:42 -0700

    Play song when it is the only song in the song queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,17 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    // this.listenTo(this.model, 'add', function() {
+    //   console.log('listening to model');
+    // });
+    this.on('add', function() {
+      
+      this.playFirst();
+    });
+  },
+
+  playFirst: function() {
+    console.log('first', this.first());
   }
 
 });
