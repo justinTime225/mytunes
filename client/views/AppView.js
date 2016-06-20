e2db0ede0323ab8fec940bd89b59b1e17a697323
Justin <justinminh225@gmail.com>
2016-06-20 15:39:16 -0700

    Does not change currently played song when enqueue

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -11,9 +11,6 @@ var AppView = Backbone.View.extend({
     this.model.on('change:currentSong', function(model){
       this.playerView.setSong(model.get('currentSong'));
     }, this);
-    // this.model.on('play', function(model){
-    //   console.log('ending in appview');
-    // }, this);
   },
 
   render: function(){
